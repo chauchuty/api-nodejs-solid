bf5e4b403c762e5ad711b699e0d61b12ae83e8ac
chauchuty <cesar.chauchuty@gmail.com>
2025-05-19 15:42:24 -0300

    add: docker-compose.yaml, prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,11 @@
 import fastify from "fastify"
+import { PrismaClient } from "generated/prisma"
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+app.get("/", async () => {
+    const users = await prisma.user.findMany()
+    return users
+})
